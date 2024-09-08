a17ab7e75f74d6be53c06e60c091311aa6944126
michaeljames-scott09 <michaeljames.scott09@gmail.com>
2024-09-08 13:51:10 -0400

    finished with part of teardown broke need to fix

--- starterCode/tip-pool/servers.test.js
+++ starterCode/tip-pool/servers.test.js
@@ -11,7 +11,30 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should not add a new server on submitServerInfo() with empty input', function() {
+    serverNameInput.value = ''
+    submitServerInfo()
+    expect(Object.keys(allServers).length).toEqual(0)
+  })
+
+  it('should update #servertable on updateServerTable()', function () {
+    submitServerInfo()
+    updateServerTable()
+
+    let liveTableList = document.querySelectorAll("#serverTable tbody tr td")
+    expect(liveTableList.length).toEqual(3)
+
+    expect(liveTableList[0].innerText).toEqual('Alice')
+    expect(liveTableList[1].innerText).toEqual('$0.00')
+    expect(liveTableList[2].innerText).toEqual('X')
+  })
+
   afterEach(function() {
     // teardown logic
+    // Expected to clear all the input fields and the table
+    serverId = 0
+    serverTbody.innerText = ""
+    allServers = {}
+    
   });
 });
